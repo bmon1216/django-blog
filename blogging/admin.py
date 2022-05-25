a1de4e9b508d7556b43a8432a7e6ccacf22a6bed
Bryan <79615060+bmon1216@users.noreply.github.com>
2022-05-25 11:04:58 -0700

    restructured the relationships between Posts and Categories models

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInLine(admin.TabularInline):
+    """Defining an InlineModelAdmin object for the relationship"""
+    # The 'through' attribute is a reference to the model that manages
+    # the many-to-many relation.
+    model = Category.posts.through
+
+
+@admin.register(Post)  # registers our PostAdmin using a decorator
+class PostAdmin(admin.ModelAdmin):
+    """A ModelAdmin for Post. Overwrites the default admin interface."""
+    # date_hierarchy = 'published_date'
+    # empty_value_display = '-empty-'
+    inlines = [CategoryInLine, ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """A ModelAdmin for Category. Declares included fields from model"""
+    fields = ('name', 'description', )
+    inlines = [CategoryInLine, ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """A ModelAdmin for Category. Declares excluded fields from model"""
+    exclude = ('posts', )
+
+
+admin.site.register(Category, CategoryAdmin)  # registers our PostAdmin
+
