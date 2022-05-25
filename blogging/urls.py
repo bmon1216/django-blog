4c3d3835f09f073c985ea87cc1c5bfd183b0c54c
Bryan <79615060+bmon1216@users.noreply.github.com>
2022-05-25 09:47:50 -0700

    added login feature

--- blogging/urls.py
+++ blogging/urls.py
@@ -3,6 +3,10 @@ from blogging.views import stub_view, list_view, detail_view
 
 # a list of routes
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>', detail_view, name="blog_detail"),
+    path('',
+         list_view,
+         name="blog_index", ),
+    path('posts/<int:post_id>',
+         detail_view,
+         name="blog_detail", ),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -39,7 +39,7 @@ def list_view(request):
 
 
 def list_view(request):
-    """A simplified version of our list_view"""
+    """A simplified version of our list_view using a render shortcut"""
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
 
@@ -48,16 +48,27 @@ def list_view(request):
     # body = template.render(context)
     # return HttpResponse(body, content_type="text/html")
     return render(request, 'blogging/list.html', context)
+    # syntax for render: render(request, template[, ctx][, ctx_instance])
 
 
 def detail_view(request, post_id):
+    """Accepts a view request and returns a response, based on post_id route"""
+    # query for published post where published_date is not 'None'
     published = Post.objects.exclude(published_date__exact=None)
 
+    # from the query, find a post that matches the post_id
     try:
         post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
 
+    # if the post does exist, push the post to render
     context = {'post': post}
+
+    # Django includes the render function, which does the following things:
+    # template - Loading a template
+    # context - Creating a context
+    # body - Generating a page body using the template & context
+    # return - Making an HTTP response from the body
     return render(request, 'blogging/detail.html', context)
 
