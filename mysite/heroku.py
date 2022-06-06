6d08ffbc277b4777a6d04fce7b15c7039d01c7c8
Bryan <79615060+bmon1216@users.noreply.github.com>
2022-06-06 15:14:27 -0700

    fixed typo

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -3,7 +3,7 @@ import os
 import dj_database_url
 
 # import everything from the current folder where heroku.py is
-from . import *
+from .settings import *
 
 DATABASES = {
     "default": dj_database_url.config(
